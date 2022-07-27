01d28ec1447980fb4cff936df9522710a45946ca
SystemLight <1466335092@qq.com>
2022-07-27 09:18:38 +0800

    feat: 升级配置版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
-const webpack5RecommendConfig = require('@systemlight/webpack-config')
+const {wcf} = require('@systemlight/webpack-config')
+// TODO: 升级5.5.6之上版本恢复提示，现阶段进入包内删除package.json的types字段恢复提示
 
-module.exports = (env, argv) => new webpack5RecommendConfig(env, argv).build().toConfig()
+module.exports = wcf({
+  buildOptions: {
+    enableFriendly: true,
+    emitPublic: false,
+    enableHash: false
+  },
+  buildConfigCallback(config) {
+    config.value = {
+      devtool: false
+    }
+  }
+})
