d51aacc55cabce7a3d071f5f52a4b81cef0e9045
SystemLight <1466335092@qq.com>
2022-09-06 17:44:05 +0800

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const {wcf} = require('@systemlight/webpack-config')
 
-module.exports = wcf({
-  enableMock: true,
-  open: false,
-  scriptExt: 'js'
-})
+module.exports = wcf(
+  {
+    enableMock: true,
+    open: false,
+    scriptExt: 'js'
+  },
+  true
+)
