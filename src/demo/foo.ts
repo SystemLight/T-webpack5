d10e4cbd85efa72ef7a96d137a775abe23741f14
Lisys <1466335092@qq.com>
2022-12-18 11:50:07 +0800

    feat: style

--- src/demo/foo.ts
+++ src/demo/foo.ts
@@ -1,7 +1,10 @@
-export function sayFoo(data) {
+import type {ResponseData} from '@/interfaces'
+
+export function sayFoo(data: ResponseData) {
   let div = document.createElement('div')
   div.innerText = 'Hello babel'
-  div.className = 'rectangle  px-[30px]'
+  // div.className = 'rectangle text-center px-[30px] fixed right-3 bottom-3'
+  div.className = 'rectangle'
   document.body.appendChild(div)
   console.log('foo', data)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,66 @@
 import '@/styles/index.scss'
 
-import React,{useEffect, useState} from 'react'
+import React, {memo, useEffect, useRef, useState} from 'react'
 import {createRoot} from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import CanvasNest from 'canvas-nest.js'
 import axios from 'axios'
 
+import type {ResponseData} from '@/interfaces'
 import {sayFoo} from '@/demo/foo'
 
-console.log(createRoot)
-// function App() {
-//   let [fooCss, setFooCss] = useState(null)
-//
-//   useEffect(function () {
-//     // mockjs api
-//     axios.post('/api/demo').then(({data}) => {
-//       sayFoo(data)
-//       setFooCss(fooCss)
-//     })
-//   }, [])
-//
-//   return (
-//     <div>{fooCss}</div>
-//   )
-// }
-//
-// const root = createRoot(document.getElementById('root')!)
-// root.render(<App/>)
+const CanvasNestComponent = memo(function () {
+  let appRef = useRef<HTMLDivElement>(null)
+
+  useEffect(function () {
+    const cn = new CanvasNest(
+      appRef.current,
+      {
+        color: '255,0,0'
+      }
+    )
+
+    return () => {
+      cn.destroy()
+    }
+  }, [appRef])
+
+  return ReactDOM.createPortal(
+    <div className={'w-[100vw] h-[100vh] fixed left-0 top-0 -z-20'} ref={appRef}></div>,
+    document.body
+  )
+}, () => true)
+
+CanvasNestComponent.displayName = 'CanvasNestComponent'
+
+function App() {
+  let [fooCss, setFooCss] = useState<string>('')
+  let [isShow, setIsShow] = useState(true)
+
+  useEffect(function () {
+    // mockjs api
+    axios.post<ResponseData>('/api/demo').then(({data}) => {
+      sayFoo(data)
+      setFooCss(data.msg)
+    })
+  }, [])
+
+  const handleToggle = () => {
+    setIsShow(!isShow)
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>{fooCss}</h1>
+        <div>
+          <button onClick={handleToggle}>toggle</button>
+        </div>
+      </div>
+      {isShow && <CanvasNestComponent/>}
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root')!)
+root.render(<App/>)
